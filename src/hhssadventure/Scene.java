2c9da2e98ed4a649bf823b7b6dfe07195d50d0b6
donet6376 <donet6376@HRH-IC0027324.wrdsb.ca>
2015-11-17 11:16:38 -0500

    scene

---
+++ src/hhssadventure/Scene.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hhssadventure;
+
+/**
+ *
+ * @author donet6376
+ */
+public class Scene {
+    
+    //picture
+    private char direction;
+    private boolean frontBlocked;
+    //nextLocation
+    //Description
+    private char nextDirection;
+    
+}
