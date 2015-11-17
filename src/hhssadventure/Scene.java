749236d0ecc4777c9d943fba27564e71ea65426b
isles3536 <isles3536@HRH-IC0027327.wrdsb.ca>
2015-11-17 11:11:10 -0500

    Ayy Lmao

---
+++ src/hhssadventure/Scene.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hhssadventure;
+
+/**
+ *
+ * @author isles3536
+ */
+public class Scene {
+    
+}
