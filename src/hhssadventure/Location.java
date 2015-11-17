95fbbfdafafbd63d13aed3b2b1448684d37739c6
paulm6438 <paulm6438@HRH-IC0027320.wrdsb.ca>
2015-11-17 11:18:28 -0500

    initial

---
+++ src/hhssadventure/Location.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hhssadventure;
+
+/**
+ *
+ * @author paulm6438
+ */
+public class Location {
+    
+}
---
+++ src/hhssadventure/Scene.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hhssadventure;
+
+/**
+ *
+ * @author paulm6438
+ */
+public class Scene {
+    
+    
+    
+    public location(){
+        
+    }
+    
+    
+    
+    
+    public nextLocation(){
+        
+    }
+    
+    public nextDirection(){
+        
+    }
+    
+    public string Description(){
+        
+        
+    }
+    
+}
