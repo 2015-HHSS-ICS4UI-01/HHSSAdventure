fb4fa14e823e9a92d3435b291de7195c4b67b7c9
naylj6470 <naylj6470@HRH-IC0027325.wrdsb.ca>
2015-11-20 10:39:53 -0500

    commit

--- src/hhssadventure/Scene.java
+++ src/hhssadventure/Scene.java
@@ -4,11 +4,19 @@
  */
 package hhssadventure;
 
+import java.awt.Graphics;
+import java.awt.Panel;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+
 /**
  *
  * @author donet6376
  */
-public class Scene {
+public class Scene extends Panel {
     
     //picture
     private char direction;
@@ -17,4 +25,37 @@ public class Scene {
     private String Description;
     private char nextDirection;
     
-}
+    BufferedImage  image;
+  public Scene() {
+  try {
+//  System.out.println("Enter image name\n");
+//  BufferedReader bf=new BufferedReader(new 
+// InputStreamReader(System.in));
+// String imageName=bf.readLine();
+  File input = new File("images/IMG_0047.JPG");
+  image = ImageIO.read(input);
+  } catch (IOException ie) {
+  System.out.println("Error:"+ie.getMessage());
+  }
+  }
+
+  public void paint(Graphics g) {
+  g.drawImage( image, 0, 0, null);
+  }
+
+  static public void main(String args[]) throws
+Exception {
+  JFrame frame = new JFrame("Display image");
+  Panel panel = new Scene();
+  frame.getContentPane().add(panel);
+  frame.setSize(1200, 800);
+  frame.setVisible(true);
+  }
+} 
+    
+    
+    
+    
+    
+    
+  
