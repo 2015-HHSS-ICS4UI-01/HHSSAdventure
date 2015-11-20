f457fafec869f2d64507d02e3539245437192b6f
janaj4926 <janaj4926@HRH-IC0027341.wrdsb.ca>
2015-11-20 11:18:22 -0500

    setup

--- src/hhssadventure/Scenes.java
+++ src/hhssadventure/Scenes.java
@@ -4,23 +4,33 @@
  */
 package hhssadventure;
 
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+
 /**
  *
  * @author janaj4926
  */
 public class Scenes {
+    BufferedImage img = null;
 
-    public void getpic() {
-        
+    public void getpic(String picName) {
+        try{
+            img = ImageIO.read(picName);
+        }catch(Exception e){
+            e.printStackTrace();
+            System.exit(0);
+        }
     }
 
     //waiting for the interface
-    public char direction(char d) {
-        
+    public int direction(String d) {
+        int c = 0;
+        return c;
     }
     
     public boolean isFrontBlocked(){
-        
+        return true;
     }
     
     public void getNextLocation(){
