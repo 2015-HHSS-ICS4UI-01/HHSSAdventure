a79c6e41a3c51789c235a3a3e621f10ea21a961d
kampn2687 <kampn2687@HRH-IC0027343.wrdsb.ca>
2015-11-20 11:21:53 -0500

    Dfs

--- src/hhssadventure/Scene.java
+++ src/hhssadventure/Scene.java
@@ -4,9 +4,31 @@
  */
 package hhssadventure;
 
+import java.util.Scanner;
+
 /**
  *
  * @author simma1980
  */
 public class Scene {
+
+    private String dir;
+    private String location;
+    private boolean frontBlocked;
+    private String nextLocation;
+    private String nextDir;
+    private String image;
+
+    public Scene(Scanner in) {
+        dir = in.next();
+        image = in.next();
+        String test = in.next();
+        if (test.equals("false")) {
+            frontBlocked = false;
+        } else {
+            frontBlocked = true;
+        }
+        
+        
+    }
 }
