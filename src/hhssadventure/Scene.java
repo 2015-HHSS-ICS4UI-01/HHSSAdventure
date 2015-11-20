b89199bcad44fa9ebd543cfe94a20adb270b299c
isles3536 <isles3536@HRH-IC0027327.wrdsb.ca>
2015-11-20 11:19:59 -0500

    Ayy Lmao

--- src/hhssadventure/Scene.java
+++ src/hhssadventure/Scene.java
@@ -4,10 +4,32 @@
  */
 package hhssadventure;
 
+import java.util.Scanner;
+
 /**
  *
  * @author isles3536
  */
 public class Scene {
+    private String name;
+    private String direction;
+    private String decrip;
+    private boolean blocked;
+    private String nextLocation;
+    private String nextDirection;
+    
+    public Scene(Scanner input){
+        name = input.nextLine();
+        direction = input.nextLine();
+        
+        if(input.nextLine() == "true"){
+            blocked = true;
+        }
+        nextLocation = input.nextLine();
+        nextDirection = input.nextLine();
+        
+    }
+    
+                
     
 }
