c900dda55bbe78d03f8ddf5e5e51a7a2908580f7
paulm6438 <paulm6438@HRH-IC0027320.wrdsb.ca>
2015-11-23 10:52:49 -0500

    initial

--- src/hhssadventure/HHSSAdventure.java
+++ src/hhssadventure/HHSSAdventure.java
@@ -5,12 +5,85 @@
  */
 package hhssadventure;
 
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author lamon
  */
 public class HHSSAdventure {
 
+    private ArrayList<Class> locations = new ArrayList<>();
+    private Interface gui;
+
+    public HHSSAdventure() {
+        //help file read
+        FileReader file = null;
+        try {
+            file = new FileReader("students/students.txt");
+        } catch (Exception e) {
+            //prints out messege
+            e.printStackTrace();
+            //stops program
+            System.exit(0);
+        }
+    
+    //creates a scanner to read file
+        Scanner input = new Scanner(file);
+
+        //keep scanning as long as there is somthing to scan
+        while (input.hasNext()) {
+            //creating a class from a file
+            Class c = new Class(input);
+            //adding the class to the list
+            classes.add(c);
+        }
+        
+        gui = new Interface(this);
+        //set the text for the next class
+        Class c = classes.get(classNum);
+        gui.setClassName(c.getCourseCode());
+        //show it
+        gui.setVisible(true);
+    }
+    
+    public void nextClass(){
+        //if classNum  is less than numClasses -1
+        if(classNum < classes.size()-1){
+            //advance to next class
+            classNum++;
+        }
+        //set the text for the next class
+        Class c = classes.get(classNum);
+        gui.setClassName(c.getCourseCode());
+    }
+    
+    
+    public void prevClass(){
+        //if classNum is >0
+        if(classNum > 0){
+            //decrease class
+            classNum--;
+        }
+        //set the text for the class name
+        Class c = classes.get(classNum);
+        gui.setClassName(c.getCourseCode());
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     /**
      * @param args the command line arguments
      */
--- src/hhssadventure/Location.java
+++ src/hhssadventure/Location.java
@@ -4,11 +4,45 @@
  */
 package hhssadventure;
 
+import java.util.Scanner;
+
 /**
  *
  * @author paulm6438
  */
 public class Location {
+
+    private String name;
+    private Scene[] scenes;
+
+    public Location(Scanner input) {
+        name = input.nextLine();
+        
+        scenes = new Scene[3];
+        
+        for (int i = 0; i < 3; i++) {
+
+            Scene s = new Scene(input);
+
+            scenes[i] = s;
+        }
+
+    }
     
+    public String getImage(){
+        return this.image;
+    }
+   
+    public boolean getIsFrontBlocked(){
+        return this.isFrontBlocked;
+    }
+    
+    public String getDescription(){
+        return this.description;
+    }
+    
+    public String getNextLocation(){
+        return this.nextLocation;
+    }
     
 }
