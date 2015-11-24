08b6e1a2bbd8387a9fd8365ff9d523b5c038fb73
janaj4926 <janaj4926@HRH-IC0027341.wrdsb.ca>
2015-11-24 10:27:20 -0500

    setup

--- src/hhssadventure/HHSSAdventure.java
+++ src/hhssadventure/HHSSAdventure.java
@@ -39,13 +39,6 @@ public class HHSSAdventure {
 //        while (input.hasNext()) {
 //            System.out.println(input.nextLine());
 //        }
-        int c = 0;
-        String[] locations = new String[1];
-        input.nextLine();
-        input.nextLine();
-        while(input.hasNext()){
-            locations[c] = input.nextLine();
-            input.
-        }
+        
     }
 }
