f6533752030ce8d910e3d36a7d8a64ee503629c1
janaj4926 <janaj4926@HRH-IC0027341.wrdsb.ca>
2015-11-25 10:30:15 -0500

    setup

--- src/hhssadventure/Locations.java
+++ src/hhssadventure/Locations.java
@@ -15,24 +15,31 @@ import java.util.Scanner;
  */
 public class Locations {
     private String name;
-
+<<<<<<< HEAD
     private Scenes[] scenes;
     
-    
+=======
+    private Scenes[] scenes = new Scenes[4];
+>>>>>>> branch 'master' of https://github.com/janaj4926/HHSSAdventure
+
+<<<<<<< HEAD
     Locations(Scanner input) {
         for(int i = 0; i < scenes.length; i++){
             scenes[i] = input.next();
         }
     }
-      
+    
+    
     public void getImage(){
         
+=======
     public Locations(Scanner input) {
         name = input.nextLine();
         for (int i = 0; i < 4; i++) {
             Scenes s = new Scenes(input);
             scenes[i] = s;
         }
+>>>>>>> branch 'master' of https://github.com/janaj4926/HHSSAdventure
     }
     
     public BufferedImage getImage(int dir){
--- src/hhssadventure/Scenes.java
+++ src/hhssadventure/Scenes.java
@@ -7,24 +7,41 @@ package hhssadventure;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.Scanner;
+<<<<<<< HEAD
 
+=======
+import javax.imageio.ImageIO;
+>>>>>>> branch 'master' of https://github.com/janaj4926/HHSSAdventure
 
 /**
  *
  * @author janaj4926
  */
 public class Scenes {
+<<<<<<< HEAD
     private String currentDirection;
     private BufferedImage image = null;
+=======
+
+    Scenes(Scanner input) {
+        
+    }
+    
+>>>>>>> branch 'master' of https://github.com/janaj4926/HHSSAdventure
 
     public BufferedImage getPic() {
         return image;
     }
 
     //waiting for the interface
+<<<<<<< HEAD
 
     public int direction(Scanner input) {
         int c = 0;
+=======
+    public int direction(String d) {
+        if
+>>>>>>> branch 'master' of https://github.com/janaj4926/HHSSAdventure
         return c;
     }
     
