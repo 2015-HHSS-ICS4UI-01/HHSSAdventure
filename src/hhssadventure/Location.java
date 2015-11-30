6fa48085362972665537468172e52594ffc24171
donet6376 <donet6376@HRH-IC0027324.wrdsb.ca>
2015-11-30 11:19:21 -0500

    DONE

--- src/hhssadventure/Location.java
+++ src/hhssadventure/Location.java
@@ -17,19 +17,35 @@ public class Location {
     private Scene[] scenes = new Scene[4];
     private String name;
 
+    /**
+     * Constructor for the locations
+     * @param constructor the scanner that is reading the files
+     */
     public Location(Scanner constructor) {
+        //store the locations' name
         name = constructor.nextLine();
+        //create scenes for all 4 direction of the location
         for (int i = 0; i < 4; i++) {
             Scene s = new Scene(constructor);
             scenes[i] = s;
+            //set each scenes' description
             s.setDescription(name + " facing " + s.getDirection());
         }
     }
 
+    /**
+     * Getter for the location name
+     * @return the name of the location
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Getter for a scenes image
+     * @param dir the direction for the scene
+     * @return the image of the scene facing a specific direction
+     */
     public BufferedImage getSceneImage(String dir) {
         for (int i = 0; i < 4; i++) {
             if (dir.equals(scenes[i].getDirection())) {
@@ -39,6 +55,11 @@ public class Location {
         return null;
     }
 
+    /**
+     * Getter for if the front is blocked
+     * @param dir the direction for the scene
+     * @return true or false
+     */
     public boolean isFrontBlocked(String dir) {
         for (int i = 0; i < 4; i++) {
             if (dir.equals(scenes[i].getDirection())) {
@@ -48,6 +69,11 @@ public class Location {
         return true;
     }
 
+    /**
+     * Getter for a scenes description
+     * @param dir the direction for the scene
+     * @return the description of the scene facing a specific direction
+     */
     public String getSceneDescription(String dir) {
         for (int i = 0; i < 4; i++) {
             if (dir.equals(scenes[i].getDirection())) {
@@ -57,6 +83,11 @@ public class Location {
         return null;
     }
 
+    /**
+     * Getter for a scenes next location
+     * @param dir the direction for the scene
+     * @return the next location of the scene facing a specific direction
+     */
     public String getSceneNextLocation(String dir) {
         for (int i = 0; i < 4; i++) {
             if (dir.equals(scenes[i].getDirection())) {
@@ -66,6 +97,11 @@ public class Location {
         return null;
     }
 
+    /**
+     * Getter for a scenes next direction
+     * @param dir the direction for the scene
+     * @return the next direction of the scene facing a specific direction
+     */
     public String getSceneNextDirection(String dir) {
         for (int i = 0; i < 4; i++) {
             if (dir.equals(scenes[i].getDirection())) {
