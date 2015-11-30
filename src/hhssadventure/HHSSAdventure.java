901acd335f7410d052316f12f50d118b732c57c8
isles3536 <isles3536@HRH-IC0027327.wrdsb.ca>
2015-11-30 11:10:17 -0500

    Ayy Lmaoooooooo

--- src/hhssadventure/HHSSAdventure.java
+++ src/hhssadventure/HHSSAdventure.java
@@ -24,6 +24,7 @@ public class HHSSAdventure {
     private String startDirection;
     private String currentDirection;
     private String currentLocation;
+    private Interface gui;
     
     public HHSSAdventure() {
         FileReader file = null;
@@ -42,7 +43,7 @@ public class HHSSAdventure {
           while(input.hasNext())
           {
              Location s = new Location(input);
-              System.out.println(s.getName());
+              
              locations.add(s);
           }
       
@@ -53,13 +54,25 @@ public class HHSSAdventure {
         
         currentDirection = startDirection;
         currentLocation = startingLocation;
-    }
         
-   
+        switchLocation("Upstairs2", "N");
+        
+        gui = new Interface();
+        gui.setVisible(true);
+        gui.setLocation(currentLocation);
+        gui.setDirection(currentDirection);
+    }
+    
     public void switchLocation(String name, String dir){
-        for(int i = 0; i < locations.size(); i++){
+        for(int i = 0; i < locations.size(); i++){  
+            if(locations.get(i).getName().equals(name) ){
+                currentLocation = name; 
+                currentDirection = dir;
+                
+            }
             
         }
+        
     }
 
     public static void main(String[] args) {
